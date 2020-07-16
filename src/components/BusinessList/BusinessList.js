7d6d4df7ad47191144de12702b1ff3faf0f29a1c
Martin Holding <martinholding89@gmail.com>
2020-07-16 18:11:52 +0100

    finished businesslist component

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,6 +4,17 @@ import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
     render () {
-        
+        (
+            <div className="BusinessList">
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+            </div>
+        )
     }
 }
+
+export default BusinessList;
