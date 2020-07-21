a437523724aae7f6513c595137764a73309c120f
Martin Holding <martinholding89@gmail.com>
2020-07-21 08:20:31 +0100

    fix image card placement, fix search bar options labels and placement

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,15 +11,16 @@ class SearchBar extends React.Component {
     renderSortByOptions() {
         return Object.keys(sortByOptions).map((sortByOption) => {
             let sortByOptionValue = sortByOptions[sortByOption];
+            return <li key={sortByOptionValue}> {sortByOption} </li>;
         });
-        return <li key= {sortByOptionValue}> {sortByOption} </li>;
+        
     }
     render() {
         return (
             <div className="SearchBar">
                 <div className="SearchBar-sort-options">
                     <ul>
-                        renderSortByOptions();
+                        {this.renderSortByOptions()};
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
